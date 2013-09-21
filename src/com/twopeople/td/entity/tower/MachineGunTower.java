82c2ccca88c95c1cb2a6fa8eba0c76d9632fedc5
Oleksii Chernikov <chernikov.lesha@gmail.com>
2013-09-21 03:02:59 +0300

    Towers Basis.

---
+++ src/com/twopeople/td/entity/tower/MachineGunTower.java
@@ -0,0 +1,12 @@
+package com.twopeople.td.entity.tower;
+
+/**
+ * Created by Alexey
+ * At 3:01 AM on 9/21/13
+ */
+
+public class MachineGunTower extends Tower {
+    public MachineGunTower(float x, float z) {
+        super(x, z);
+    }
+}
