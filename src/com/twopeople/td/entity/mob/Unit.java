c7e80b72d72e4dabfbc094910f3be0562ecf1d34
Oleksii Chernikov <chernikov.lesha@gmail.com>
2013-09-22 03:20:07 +0300

    Parseable Units Basis.

---
+++ src/com/twopeople/td/entity/mob/Unit.java
@@ -0,0 +1,14 @@
+package com.twopeople.td.entity.mob;
+
+import com.twopeople.td.world.World;
+
+/**
+ * Created by Alexey
+ * At 3:19 AM on 9/22/13
+ */
+
+public class Unit extends Mob {
+    public Unit(World world, float x, float z) {
+        super(world, x, z, 0, 0);
+    }
+}
